80be742d2cfe766dbf14210886db97582fe294d4
M. Balayssac Antoine <abalayss@insa-10583>
2019-01-30 20:42:14 +0100

    change timeout

--- Clavard/src/reseau/ClientTCP.java
+++ Clavard/src/reseau/ClientTCP.java
@@ -11,9 +11,9 @@ import java.lang.InterruptedException;
 public class ClientTCP {
     private Socket socket_;
 
-    //port d'écoute du srv auquel le client doit se connecter
+    //port d'ï¿½coute du srv auquel le client doit se connecter
     private int port_;
-    //port d'écoute du srv auquel le client doit se connecter
+    //port d'ï¿½coute du srv auquel le client doit se connecter
     private InetAddress address_;
     private TCPListener listener_;
     private InterfaceReseau ir_;
@@ -25,18 +25,18 @@ public class ClientTCP {
 	port_=port;
 	ir_=ir;
 	pseudoServeur_=pseudoServeur;
-       	System.out.println("Client : crée pour contacter "+pseudoServeur_);
+       	System.out.println("Client : crï¿½e pour contacter "+pseudoServeur_);
     }
     
     public void startClient() {
 	try{
-	    System.out.println("Client démarre.");
+	    System.out.println("Client dï¿½marre.");
 	    socket_ = new Socket(address_, port_);
 	    //socket_.setSoTimeout(50000);
 
 	    bbTCP_ = new BlablaTCP(pseudoServeur_, ir_, socket_);
 	    
-	    //on construit et démarre le listener
+	    //on construit et dï¿½marre le listener
 	    listener_ = new TCPListener(bbTCP_, socket_);
 	    Thread threadListener = new Thread(listener_);
 	    threadListener.start();
@@ -44,15 +44,15 @@ public class ClientTCP {
 	    /*on envoie notre pseudo pour Ãªtre reconnu
 	      on fait attendre un peu ce thread
 	      pour laisser le temps au serveur d'Ãªtre en mesure de lire le message*/
-	    Thread.sleep(2000);
+	    Thread.sleep(100);
 	    //on envoie notre pseudo pour s'identifier
 	    bbTCP_.envoyerPseudoLocal();
 	    ir_.nouveauCorrespondant(pseudoServeur_, bbTCP_);
 	} catch (UnknownHostException e) {
-	    System.out.println("PB starClient : hôte inconnu.");
+	    System.out.println("PB starClient : hï¿½te inconnu.");
 	    e.printStackTrace();
 	} catch (IOException e) {
-	    System.out.println("PB starClient : créaion socket.");
+	    System.out.println("PB starClient : crï¿½aion socket.");
 	    e.printStackTrace();
 	}catch (InterruptedException e) {
 	    System.out.println("PB starClient : Thread.sleep().");
